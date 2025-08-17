5c1a75a3fd20a4c71ab0e9e178ffc029b88f9754
Anton Vakhania <vakhaniaanton@gmail.com>
2025-08-17 21:07:38 +0300

    Добавляет решение для задания 'Шаблонизируй это'

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import PointsModel from './model/points-model.js';
 import TablePresenter from './presenter/table-presenter';
 import TripInfoView from './view/trip-info-view.js';
 import ListFilterView from './view/list-filter-view.js';
@@ -8,9 +9,11 @@ const tripMainElement = document.querySelector('.trip-main');
 const filtersContainerElement = tripMainElement.querySelector('.trip-controls__filters');
 const pointsContainerElement = document.querySelector('.trip-events');
 
-const tablePresenter = new TablePresenter({ container: pointsContainerElement });
+const pointsModel = new PointsModel();
+const tablePresenter = new TablePresenter({ container: pointsContainerElement, pointsModel });
 
 render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
 render(new ListFilterView(), filtersContainerElement);
 
+pointsModel.init();
 tablePresenter.init();
---
+++ src/utils.js
@@ -0,0 +1,36 @@
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import { BasePrice, DateFormat } from './const.js';
+
+dayjs.extend(duration);
+
+const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
+
+const humanizeDateTime = (date) => dayjs(date).format(DateFormat.DAY_MONTH_YEAR_HOUR_MINUTE);
+
+const humanizeDate = (date) => dayjs(date).format(DateFormat.MONTH_DAY);
+
+const humanizeTime = (date) => dayjs(date).format(DateFormat.HOUR_MINUTE);
+
+const getTimeDifference = (dateFrom, dateTo) => {
+  const date1 = dayjs(dateFrom);
+  const date2 = dayjs(dateTo);
+  const datesDifference = date2.diff(date1);
+  const durationObject = dayjs.duration(datesDifference);
+  if (durationObject.asHours() < 1) {
+    return durationObject.format(DateFormat.DURATION_MINUTE);
+  }
+  if (durationObject.asDays() < 1) {
+    return durationObject.format(DateFormat.DURATION_HOUR_MINUTE);
+  }
+  return durationObject.format(DateFormat.DURATION_DAY_HOUR_MINUTE);
+};
+
+const getBasePrice = (a = BasePrice.MIN, b = BasePrice.MAX) => {
+  const min = Math.ceil(Math.min(a, b));
+  const max = Math.floor(Math.max(a, b));
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export { humanizeDate, humanizeTime, humanizeDateTime, getTimeDifference, getRandomArrayElement, getBasePrice };

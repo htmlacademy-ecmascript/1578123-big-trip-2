532bdd9e3c6fbdc4d9ad34842ac22cca69a6dd84
Anton Vakhania <vakhaniaanton@gmail.com>
2025-09-04 11:17:00 +0300

    Разделяет модели

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,31 +1,11 @@
-import { destinations } from '../mock/destinations.js';
-import { offers } from '../mock/offers.js';
 import { getRandomPoint } from '../mock/points.js';
 import { POINTS_COUNT } from '../const.js';
 
 class PointsModel {
-  constructor() {
-    this.points = [];
-    this.destinations = [];
-    this.offers = [];
-  }
+  #points = Array.from({ length: POINTS_COUNT }, getRandomPoint);
 
-  init() {
-    this.points = Array.from({length: POINTS_COUNT}, getRandomPoint);
-    this.destinations = destinations;
-    this.offers = offers;
-  }
-
-  getPoints() {
-    return this.points;
-  }
-
-  getDestinations() {
-    return this.destinations;
-  }
-
-  getOffers() {
-    return this.offers;
+  get points() {
+    return this.#points;
   }
 }
 
